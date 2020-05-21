44c644bdf86403cf2f677160172be08b707c6351
James Heaton <jwheatonumich@gmail.com>
2020-05-20 21:05:52 -0500

    Notes 3

---
+++ ES6-Notes-3.jsx
@@ -0,0 +1,6 @@
+//Using a backticks `` around text can include linebreaks and variables directly in the text
+const person = {name: "Zodiac Hasbro",age: 56};
+  
+// ${} is used for variables. ! is used to break the line.
+const greeting = `Hello, my name is ${person.name}!
+I am ${person.age} years old.`;
