8d41085446a05e89e881c0d1d7d1ba564a7b6b08
James Heaton <jwheatonumich@gmail.com>
2020-05-20 21:14:59 -0500

    Update ES6-Notes-3.jsx

--- ES6-Notes-3.jsx
+++ ES6-Notes-3.jsx
@@ -4,3 +4,18 @@ const person = {name: "Zodiac Hasbro",age: 56};
 // ${} is used for variables. ! is used to break the line.
 const greeting = `Hello, my name is ${person.name}!
 I am ${person.age} years old.`;
+
+//ES6 has shorthand for defining an object where the attributes have the same name as function inputs
+const getMousePosition = (x, y) => ({
+    x: x, //Here we do it the slow way and define each attribute from an input
+    y: y
+  });
+
+const getMousePosition = (x, y) => ({ x, y }); //This is the shortcut that createx x:x and y:y
+
+//Example using the above shorthand
+let cat = 'Miaow';
+let dog = 'Woof';
+let bird = 'Peet peet';
+
+let someObject = {cat,dog,bird} //{cat: "Miaow", dog: "Woof", bird: "Peet peet"}
