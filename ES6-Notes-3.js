82f0175b53de93b76d5f654f078f40eafda8df0a
jwheaton <jwheatonumich@gmail.com>
2020-05-27 19:36:43 -0500

    Revised file type

--- ES6-Notes-3.js
+++ ES6-Notes-3.js
@@ -19,3 +19,40 @@ let dog = 'Woof';
 let bird = 'Peet peet';
 
 let someObject = {cat,dog,bird} //{cat: "Miaow", dog: "Woof", bird: "Peet peet"}
+
+//A constructor function can be used to create multiple similar objects
+//Constructor that creates objects based on 4 inputs
+function Person(first, last, age, eye) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eye;
+}
+//Uses the constructor funnction with the "new" keyword to create objects for 2 people
+var myFather = new Person("John", "Doe", 50, "blue");
+var myMother = new Person("Sally", "Rally", 48, "green");
+
+//ES6 uses "Class" to create constructor functions more easily
+class Person {
+  constructer (first, last, age, eye){
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+  }
+}
+
+//get and set can be used to define how to output or modify data in variables in an object
+class Book { //Fnction can be used to enter an author
+  constructor(author) {
+    this._author = author;
+  }
+  // getter
+  get writer() { //Defines how to get data out of the object
+    return this._author;
+  }
+  // setter
+  set writer(updatedAuthor) { //Defines how to modify value of variable in the function
+    this._author = updatedAuthor;
+  }
+}
